5c1292fcac758d25097c97a846a01be99743c58f
PrakashRavichandran <prakashravichandran2530@gmail.com>
2022-12-05 14:49:07 +0530

    first commit

---
+++ content/posts/test-post-with-tag-example-1.md
@@ -0,0 +1,11 @@
+---
+title: Test post with tag example 1
+date: 2022-10-15 21:20:36+07:00
+featuredImage: ../images/cooking-preparation.jpg
+tags:
+  - General
+author: Fathan Fisabilillah
+featured: false
+published: false
+---
+T﻿est post with new example 1
---
+++ src/config/headerMenu.js
@@ -0,0 +1,34 @@
+module.exports = {
+  headerMenu: [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "Code",
+      url: "/tag/code",
+	  subMenu: [
+        {
+          name: "Javascript",
+          url: "/tag/javascript/",
+        },
+        {
+          name: "Wordpress",
+          url: "/tag/wordpress/",
+        },
+      ],
+    },
+    {
+      name: "Management",
+      url: "/tag/management/",
+    },
+	{
+	  name: "Engineering Lead Materials (Books, Podcast, Links)",
+	  url: "/engineering-lead-materials/",
+	},
+    {
+      name: "About me",
+      url: "/contact/",
+    },
+  ],
+}
